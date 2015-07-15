abf423b8db13386c411044dd7571ef382ceb586a
Al Maline <amaline@yahoo.com>
2015-07-15 12:26:33 -0400

    updated sqs_poll.rb to be more generic so it just routes to any endpoint using application/json

--- sqs_poll.rb
+++ sqs_poll.rb
@@ -8,11 +8,11 @@ require 'json'
 q	= "slack_receive_queue"
 useast 	= "us-east-1"
 
-if ENV['SLACK_URL'].nil?
-  fail "SLACK_URL environmental variable must be set"
+if ENV['ROUTER_ENDPOINT'].nil?
+  fail "ROUTER_ENDPOINT environmental variable must be set"
 end
 
-endpoint = URI.parse ENV['SLACK_URL']
+endpoint = URI.parse ENV['ROUTER_ENDPOINT']
 #endpoint = URI.parse "http://requestb.in/108ulae1"
 
 
@@ -31,9 +31,9 @@ poller.poll do |msg|
 
  parsed = JSON.parse(msg.body)
 
- if parsed["channel_name"] != "chatops"
-   next #skip non chatops channels
- end
+ #if parsed["channel_name"] != "chatops"
+   #next #skip non chatops channels
+ #end
 
  puts "id	 = #{parsed['user_id']}"
  puts "message   = #{parsed['text']}"
@@ -41,7 +41,7 @@ poller.poll do |msg|
  # no infinite loops of messages by filtering user_id for USLACKBOT
  # only messages that begin with %% will be considered commands
 
- if parsed["user_id"] != "USLACKBOT" && parsed["text"] =~ /^%%/	
+ if parsed["user_id"] != "USLACKBOT" # && parsed["text"] =~ /^%%/	
 
    payload={"text":"message received {#{parsed['text']}}"}
 
@@ -50,8 +50,15 @@ poller.poll do |msg|
    http.ssl_version = :TLSv1
 
    req = Net::HTTP::Post.new(endpoint.request_uri)
-   req.set_form_data ( {"payload" => payload.to_json } )
-   req["Content-Type"] = 'application/x-www-form-urlencoded'
+   #req.set_form_data ( {"payload" => payload.to_json } )
+   req.body (payload.to_json)
+   
+   #req["Content-Type"] = 'application/x-www-form-urlencoded'
+   req["Content-Type"] = 'application/json'
+
+   if ENV['X_AUTH_TOKEN'].nil?
+	req["X-Auth-Token"] = ENV['X_AUTH_TOKEN']
+   end
 
    resp=http.request(req)
    case resp
